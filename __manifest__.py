c5c0107cbd9bf72ad6c9b9e899b14b8ac71189b0
trilok <ram@hk.com>
2019-02-13 13:06:01 +0530

    updated 1:05pm feb 13, 2019

--- __manifest__.py
+++ __manifest__.py
@@ -26,9 +26,45 @@
 
         # Sequence
         "sequence/person.xml",
+        "sequence/product.xml",
+        "sequence/store.xml",
+
+        # Hr
+        "views/hr/employee.xml",
+        "views/hr/category.xml",
+        "views/hr/department.xml",
+        "views/hr/designation.xml",
+        "views/hr/experience.xml",
+        "views/hr/identities.xml",
+        "views/hr/address.xml",
+        "views/hr/qualification.xml",
+
+        # Product
+        "views/product/product.xml",
+        "views/product/product_group.xml",
+        "views/product/sub_group.xml",
+        "views/product/uom.xml",
+        "views/product/tax.xml",
+        "views/product/category.xml",
+
+        # Store
+        "views/stock/location.xml",
+        "views/stock/warehouse.xml",
+        # "views/store/stock_adjustment.xml",
+        # "views/store/store_request.xml",
+        # "views/store/store_issue.xml",
+        # "views/store/store_return.xml",
+        # "views/store/store_accept.xml",
+        # "views/store/arc_move.xml",
+        "views/configuration/store_config.xml",
+
+        # Asserts
 
         # Menu
         "views/menu/main_menu.xml",
+        "views/menu/hr.xml",
+        "views/menu/inventory.xml",
+        # "views/menu/asserts.xml",
 
 
     ],
--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from . import base
+from . import general
+from . import configuration
 from . import account
 from . import person
+from . import patient
+from . import hr
+from . import time_management
+from . import leave_management
+from . import product
+from . import stock
